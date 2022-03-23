cfbecb448f7deae501715e67b3121f5c14000ffa
jefesant <jeferson.santos@capgemini.com>
2022-03-23 12:56:03 -0300

    ProductEvent

--- src/main/java/com/wiredbraincoffee/productapiannotarion/controller/ProductController.java
+++ src/main/java/com/wiredbraincoffee/productapiannotarion/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.wiredbraincoffee.productapiannotarion.controller;
 
 import com.wiredbraincoffee.productapiannotarion.model.Product;
+import com.wiredbraincoffee.productapiannotarion.model.ProductEvent;
 import com.wiredbraincoffee.productapiannotarion.repository.ProductRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 
 /**
@@ -71,6 +75,12 @@ public class ProductController {
     public Mono< ? > deleteAll() {
         return productRepository.deleteAll()
                 .then(Mono.just(ResponseEntity.ok().build()));
+    }
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux< ProductEvent > getEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> new ProductEvent(sequence, "Product Event " + sequence));
     }
 }
 
